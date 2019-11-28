d84a4b929e14782ec32b5d1986e0ff0eca9899eb
William Owen <william@william-owen.co.uk>
2019-11-28 10:31:23 +0000

    Update packages and clean interface

--- index.js
+++ index.js
@@ -7,37 +7,37 @@
 
 \* ----------------------- */
 
-const program = require('commander');
-const chalk = require('chalk');
-const moment = require('moment');
-const low = require('lowdb');
-const settings = require('user-settings').file('.hulo-settings');
-const inquirer = require('inquirer');
-const path = require('path');
-const gitUserName = require('git-user-name');
-const gitBranch = require('git-branch');
-const gitRepoName = require('git-repo-name');
-const homedir = process.env.HOME || process.env.USERPROFILE
-const filepath = path.join(homedir, 'hulo.db');
-const fs = require('fs');
+const commander = require('commander')
+const chalk = require('chalk')
+const moment = require('moment')
+const low = require('lowdb')
+const settings = require('user-settings').file('.hulo-settings')
+const inquirer = require('inquirer')
+const gitUserName = require('git-user-name')
+const gitBranch = require('git-branch')
+const gitRepoName = require('git-repo-name')
+const fs = require('fs')
+const FileSync = require('lowdb/adapters/FileSync')
+
+const program = new commander.Command()
+const prompt = inquirer.createPromptModule()
 
 hulo = {}
 
 // Define the database
-
-hulo.db = low(filepath);
+const adapter = new FileSync('hulo.db')
+hulo.db = low(adapter)
 
 hulo.db.defaults({
 
 	log: [],
-	track: [],
-	count: []
+	task: [],
 
 }).write()
 
 // Define User information
 
-hulo.username = settings.get('username');
+hulo.username = settings.get('username')
 
 // Define Git information
 
@@ -45,75 +45,87 @@ hulo.git = {}
 
 if (fs.existsSync('.git')) {
 
-	const CurrentGitUserName = gitUserName();
-	const CurrentGitBranchName = gitBranch.sync();
-	const CurrentGitRepoName = gitRepoName.sync();
+	const CurrentGitUserName = gitUserName()
+	const CurrentGitBranchName = gitBranch.sync()
+	const CurrentGitRepoName = gitRepoName.sync()
 
-	hulo.git.username = CurrentGitUserName ? CurrentGitUserName : 'Unknown Git User';
-	hulo.git.branch = CurrentGitBranchName ? CurrentGitBranchName : 'Unknown Git Branch';
-	hulo.git.repo = CurrentGitRepoName ? CurrentGitRepoName : 'Unknown Git Repo';
+	hulo.git.username = CurrentGitUserName ? CurrentGitUserName : 'Unknown Git User'
+	hulo.git.branch = CurrentGitBranchName ? CurrentGitBranchName : 'Unknown Git Branch'
+	hulo.git.repo = CurrentGitRepoName ? CurrentGitRepoName : 'Unknown Git Repo'
 
 }
 
 // Console message helper functions
 
-hulo.console = {};
+hulo.console = {
 
-hulo.console.out = (message) => {
+	out: (message) => {
 
-	// This is a helper function for formatting a message to the console.
+		// This is a helper function for formatting a message to the console.
+	
+		console.log(`${chalk.bold('Hulo:')} ${message}`)
+	
+		return true
+	
+	},
 
-	console.log(`${chalk.bold('Hulo:')} ${message}`);
+	done: (message) => {
 
-	return true;
+		// This is a helper function to send a done message to the console.
+	
+		console.log(`${chalk.bold('hulo:')} ${chalk.green(message)}`)
+	
+		return true
+	
+	},
 
-};
+	problem: (message) => {
 
-hulo.console.done = (strMessage) => {
+		// This is a helper function to send a done message to the console.
+	
+		console.log(`${chalk.bold('hulo:')} ${chalk.red(message)}`)
+	
+		return true
+	
+	}
 
-	// This is a helper function to send a done message to the console.
-
-	console.log(`${chalk.bold('hulo:')} ${chalk.green(strMessage)}`);
-
-	return true;
-
-};
+}
 
 // Message formatting helper functions
 
-hulo.format = {};
+hulo.format = {
 
-hulo.format.value = (value) => {
+	value: (value) => {
 
-	// Helper function to output value
+		// Helper function to output value
+	
+		return chalk.blue(value)
+	
+	},
 
-	return chalk.blue(value);
+	problem: (value) => {
 
-};
+		// Helper function to output problem
+	
+		return chalk.red(value)
+	
+	}
 
-hulo.format.problem = (value) => {
-
-	// Helper function to output problem
-
-	return out(chalk.red(value));
-
-};
+}
 
 // Set the username as a user preference
 
 hulo.setUserName = (newUsername) => {
 
-	hulo.username = newUsername;
-	settings.set('username', hulo.username);
-	hulo.console.out(`Your username is set to ${hulo.username}`);
+	hulo.username = newUsername
+	settings.set('username', hulo.username)
+	hulo.console.out(`Your username is set to ${hulo.username}`)
 
-	return true;
+	return true
 
-};
+}
 
 hulo.ensureUsernameSet = (next) => {
-
-	const prompt = inquirer.createPromptModule();
 
 	if ( ( !hulo.username || hulo.username === "" ) && !hulo.gitUsername ) {
 
@@ -125,18 +137,18 @@ hulo.ensureUsernameSet = (next) => {
 
 		}]).then(answers => {
 
-			hulo.setUserName( answers.username );
-			next();
+			hulo.setUserName( answers.username )
+			next()
 
-		});
+		})
 
 	} else {
 
-		next();
+		next()
 
 	}
 
-};
+}
 
 
 // LOG
@@ -159,18 +171,18 @@ hulo.log = (message) => {
 				timestamp: moment().format(),
 				message: message
 			})
-			.write();
+			.write()
 
-		hulo.console.done('Message logged.');
+		hulo.console.done('Message logged.')
 
-	});
+	})
 
 }
 
 // Command line UI description
 
 program
-	.version('0.1.0 alpha')
+	.version('1.0.4 alpha', '-v, --version', 'output the current version');
 
 program
 	.command('log [message]')
@@ -179,13 +191,11 @@ program
 
 		if (message) {
 
-			hulo.log( message );
+			hulo.log( message )
 
 		} else {
 
-			const messagePrompt = inquirer.createPromptModule();
-
-			messagePrompt([{
+			prompt([{
 
 				type: 'editor',
 				name: 'logMessage',
@@ -193,139 +203,146 @@ program
 
 			}]).then(answers => {
 
-				hulo.log( answers.logMessage );
+				hulo.log( answers.logMessage )
 
-				next();
-
-			});
+			})
 
 		}
 
-	});
+	})
 
 program
-	.command('last [count]')
+	.command('read [count]')
+	.alias('last')
+	.option("-v, --verbose", "Verbose output")
+	.option("-a, --all", "Read all")
 	.description('Output the last Hulo log entry. Optional count for number of items to return.')
-	.action(function(count){
+	.action(function(count, options){
 
-		count = parseInt(count) || 1;
+		count = parseInt(count) || 1
 
-		let result = hulo.db.get('log')
-			.takeRight(count)
-			.value();
+		let result = options.all ? hulo.db.get('log').value() : hulo.db.get('log').takeRight(count).value()
 
 		result.map(logItem => {
 
-			let gitBranchDetails = "";
-			let usernameString = "";
-			let systemDetails = "";
+			let usernameString = ""
 
-			if ( logItem.git.repo ) {
+			if (options.verbose) {
 
-				gitBranchDetails += chalk.green('Repo:') + ' ' + chalk.blue(logItem.git.repo) + ' ';
+				let gitBranchDetails = ""
+				let systemDetails = ""
 
+				if ( logItem.git.repo ) {
+
+					gitBranchDetails += chalk.green('Repo:') + ' ' + chalk.blue(logItem.git.repo) + ' '
+
+				}
+
+				if ( logItem.git.branch ) {
+
+					gitBranchDetails += chalk.green('Branch:') + ' ' + chalk.blue(logItem.git.branch) + ' '
+
+				}
+
+				if ( logItem.system.path ) {
+
+					systemDetails += chalk.green('Path:') + ' ' + chalk.blue(logItem.system.path) + ' '
+
+				}
+
+				console.info(`${chalk.green(moment(logItem.timestamp).format('dddd, MMMM Do YYYY, h:mm:ss a'))}`)
+
+				if (gitBranchDetails !=="") {
+
+					console.info(gitBranchDetails)
+
+				}
+
+				if (systemDetails !=="") {
+
+					console.info(systemDetails)
+
+				}
+				
+				if (hulo.username && hulo.git.username !== undefined ) {
+
+					usernameString = chalk.blue(hulo.username) + ' ' + chalk.green('(') + chalk.blue(hulo.git.username) + chalk.green(')')
+	
+				} else if (hulo.username) {
+	
+					usernameString = chalk.blue(hulo.username)
+	
+				}
+
+				console.info(`${usernameString} ${chalk.green(`wrote:`)}`)
+				console.info(`${logItem.message}`)
+
+			} else {
+
+				if (hulo.username) {
+	
+					usernameString = chalk.blue(hulo.username)
+	
+				}
+	
+				console.info(`${chalk.green(moment(logItem.timestamp).format('D-MM-YY hh:mm'))} ${usernameString} ${chalk.green(`wrote:`)} ${logItem.message}`)
+	
 			}
-
-			if ( logItem.git.branch ) {
-
-				gitBranchDetails += chalk.green('Branch:') + ' ' + chalk.blue(logItem.git.branch) + ' ';
-
-			}
-
-			if (hulo.username && hulo.git.username !== undefined ) {
-
-				usernameString = chalk.blue(hulo.username) + ' ' + chalk.green('(') + chalk.blue(hulo.git.username) + chalk.green(')');
-
-			} else if (hulo.username) {
-
-				usernameString = chalk.blue(hulo.username)
-
-			}
-
-			if ( logItem.system.path ) {
-
-				systemDetails += chalk.green('Path:') + ' ' + chalk.blue(logItem.system.path) + ' ';
-
-			}
-
-			console.info(`${chalk.green(moment(logItem.timestamp).format('dddd, MMMM Do YYYY, h:mm:ss a'))}`);
-
-			if (gitBranchDetails !=="") {
-
-				console.info(gitBranchDetails);
-
-			}
-
-			if (systemDetails !=="") {
-
-				console.info(systemDetails);
-
-			}
-
-			console.info(usernameString + chalk.green(' wrote: -'));
-			console.info(`${logItem.message}`);
-			console.info(`-`);
+			
 
 		})
 
-	});
+	})
 
 program
-	.command('count <item> [number]')
-	.description('Count an item.')
-	.action(function(item, number){
+	.command('task [taskName]')
+	.option("-s, --start", "End current task and start a new task")
+	.option("-e, --end", "End a task")
+	.description('Track a task')
+	.action(function(taskName, options){
 
-		// number = number || 1;
-		// out(`Counting ${value(number)} ${value(item)} at ${timeStamp()}`);
-		// done();
-		hulo.console.problem('Count not currently implemented.');
+		let result = hulo.db.get('task').takeRight(1).value() || []
 
-	});
+		if(options.end) {
 
-program
-	.command('howmany <item>')
-	.description('Get the current count for an item.')
-	.action(function(item, number){
-
-		// number = number || 1;
-		// out(`Counting ${value(number)} ${value(item)} at ${timeStamp()}`);
-		// done();
-		hulo.console.problem('howmany not currently implemented.');
-
-	});
-
-program
-	.command('username [newUsername]')
-	.description('Get the current username.')
-	.action(function(newUsername){
-
-		if (newUsername) {
-
-			hulo.setUserName(newUsername);
-
-		} else {
-
-			hulo.ensureUsernameSet(() => {
-
-				hulo.console.out(`Your username is set to ${hulo.username}`);
-				hulo.console.out(`Your Git username is set to ${hulo.gitUsername}`);
-
-			});
+			// End a task
+			hulo.console.problem('End task not implemented  yet.')
 
 		}
 
-	});
+		if(options.start) {
+			
+			// End a task and start a new one, or just start a new one
+			hulo.console.problem(`Start task not implemented yet so can't start ${taskName}`)
 
-program
-	.command('track <item> <number>')
-	.description('Keep a record of a value over time.')
-	.action(function(item, number){
+		}
 
-		// out(`Tracking ${value(item)} at new value ${value(number)} at ${timeStamp()}`);
-		// done();
+		if (!options.start && !options.end || options.start && options.end ) {
+			
+			// Read a task
 
-		problem('Track not currently implemented.');
+			hulo.console.problem('Read task no implemented.')
 
-	});
+			if (!result.length) {
 
-program.parse(process.argv);
+				hulo.console.problem('No tasks have been recorded yet.')
+
+			} else {
+
+				if(result.end) {
+
+					hulo.console.problem('No tasks currently tracked.')
+
+				} else {
+
+					hulo.console.out(`${result.taskName} was started ${moment(result.start).format('D-MM-YY hh:mm')} (${moment(result.start).duration()})`)
+
+				}
+
+			}
+
+		}	
+
+	})
+
+program.parse(process.argv)
